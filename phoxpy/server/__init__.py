e24b4dd8bac1ecd868cf269bbfa99dc130b9e59c
Alexander Shorin <Alexander Shorin@localhost>
2011-11-15 11:48:24 +0300

    Move MockHttpSession to server package. Remove old server implementation.

--- phoxpy/server/__init__.py
+++ phoxpy/server/__init__.py
@@ -7,8 +7,18 @@
 # you should have received as part of this distribution.
 #
 
+from cStringIO import StringIO
 from phoxpy.server.auth import AuthExt
 from phoxpy.server.main import BaseLisServer
+
+class MockHttpSession(object):
+
+    def __init__(self, server=None):
+        self.server = server or SimpleLISServer('0.0', '00000')
+
+    def request(self, method, url, body=None, headers=None, credentials=None,
+                _num_redirects=0):
+        return 200, {}, StringIO(str(self.server.dispatch(body)))
 
 
 class SimpleLISServer(BaseLisServer):
