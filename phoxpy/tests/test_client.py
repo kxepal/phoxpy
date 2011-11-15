7e025a5fa40d5f7ba5721ad01951ad2020ed2485
Alexander Shorin <kxepal@gmail.com>
2011-11-15 11:48:34 +0300

    Update tests for new environment.

--- phoxpy/tests/test_client.py
+++ phoxpy/tests/test_client.py
@@ -9,22 +9,28 @@
 
 import unittest
 from phoxpy import client
-from phoxpy.tests.lisserver import MockHttpSession
+from phoxpy.server import MockHttpSession, SimpleLISServer
+
 
 class SessionTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.server = SimpleLISServer('4.2', '31415')
+        self.server.ext_auth.add_license('foo-bar-baz')
+        self.server.ext_auth.add_user('John', 'Doe')
 
     def test_login(self):
         session = client.Session(login='John', password='Doe',
                                  client_id='foo-bar-baz')
         self.assertFalse(session.is_active())
-        session.open('localhost', http_session=MockHttpSession())
+        session.open('localhost', http_session=MockHttpSession(self.server))
         self.assertTrue(session.is_active())
 
     def test_logout(self):
         session = client.Session(login='John', password='Doe',
                                  client_id='foo-bar-baz')
         self.assertFalse(session.is_active())
-        session.open('localhost', http_session=MockHttpSession())
+        session.open('localhost', http_session=MockHttpSession(self.server))
         self.assertTrue(session.is_active())
         session.close()
         self.assertFalse(session.is_active())
