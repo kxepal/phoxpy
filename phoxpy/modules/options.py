2be561af72d6e9a09cde57dd3d669c1cc8d42d02
Alexander Shorin <kxepal@gmail.com>
2011-11-17 00:55:09 +0300

    Add support for options-get request.

---
+++ phoxpy/modules/options.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Alexander Shorin
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+from phoxpy.messages.options import OptionsGet
+
+__all__ = ['load']
+
+def load(session):
+    """Loads global config options.
+    
+    :param session: Active :ref:`~phox.client.Session` instance.
+    
+    :return: Config options as is.
+    :rtype: dict
+    """
+    resp = session.request(body=OptionsGet())
+    config = {}
+    for item in resp['']: # unnamed sequence
+        config[item['code']] = item['value']
+    return config
