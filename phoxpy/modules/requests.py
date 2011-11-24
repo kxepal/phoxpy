eb226be93d01c1baa0d56a268ebf659cfd6645d0
Alexander Shorin <kxepal@gmail.com>
2011-11-25 01:04:45 +0300

    Add support for request info loading and quering registation journal.

---
+++ phoxpy/modules/requests.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Alexander Shorin
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+from phoxpy.messages.requests import RequestInfo
+from phoxpy.messages.journal import RegistrationJournalFilter, \
+                                    RegistrationJournalRequest
+
+__all__ = ['load', 'select', 'changes']
+
+def load(session, idx):
+    """Loads request information by provided id.
+
+    :param session: Active session instance.
+    :type session: :class:`~phoxpy.client.Session`
+
+    :param idx: Request id.
+    :type idx: str
+
+    :returns: Request information.
+    :rtype: dict
+    """
+    msg = RequestInfo(request=idx)
+    resp = session.request(body=msg)
+    return resp.unwrap()
+
+def select(session, filter=None, **options):
+    """Selects requests from registration journal.
+
+    :param session: Active session instance.
+    :type session: :class:`~phoxpy.client.Session`
+
+    :param filter: Predefined requests filter.
+    :type filter: :class:`~phoxpy.modules.requests.RegistrationJournalFilter`
+
+    :param options: Custom additional filter options. See
+                    :class:`~phoxpy.modules.requests.RegistrationJournalFilter`
+                    for supported keys.
+
+    :yields: Registration journal rows as dict.
+    """
+    if filter is None:
+        filter = RegistrationJournalFilter(**options)
+    else:
+        filter.update(**options)
+    msg = RegistrationJournalRequest(filter=filter)
+    resp = session.request(body=msg)
+    for row in resp['Request']:
+        yield row.unwrap()
+
+def changes(session,  timestamp=0):
+    """Generates changes in registration journal since specified timestamp.
+
+    :param session: Active session instance.
+    :type session: :class:`~phoxpy.client.Session`
+
+    :param timestamp: Prepared requests filter.
+    :type timestamp: int
+
+    :yields: Registration journal rows as dict.
+    """
+    while True:
+        for item in select(session, last_timestamp=timestamp):
+            if timestamp < item['timestamp']:
+                timestamp = item['timestamp']
+            yield item
---
+++ phoxpy/tests/modules/test_requests.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011 Alexander Shorin
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+#
+
+import types
+import unittest
+from phoxpy import client
+from phoxpy.server import MockHttpSession, SimpleLISServer
+from phoxpy.modules import requests
+
+class RequestsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        server = SimpleLISServer('4.2', '31415')
+        server.ext_auth.add_license('foo-bar-baz')
+        server.ext_auth.add_user('John', 'Doe')
+        server.ext_reqs.set({'id': 'foo', 'data': 1})
+        server.ext_reqs.set({'id': 'bar', 'data': 2})
+
+        session = client.Session(login='John', password='Doe',
+                                 client_id='foo-bar-baz')
+        session.open('localhost', http_session=MockHttpSession(server))
+        self.session = session
+        self.server = server
+
+    def test_load(self):
+        req = requests.load(self.session, 'foo')
+        self.assertEqual(req['data'], 1)
+
+    def test_select_all(self):
+        items = requests.select(self.session)
+        self.assertEqual(
+            sorted(list(self.server.ext_reqs.db.values())),
+            sorted(list(items))
+        )
+
+    def test_changes(self):
+        items = requests.changes(self.session, 123)
+        self.assertTrue(isinstance(items, types.GeneratorType))
+        foo = items.next()
+        self.assertEqual('foo', foo['id'])
+        self.assertEqual('bar', items.next()['id'])
+        self.server.ext_reqs.set({'id': 'bar', 'data': 5}, 10)
+        self.server.ext_reqs.set({'id': 'foo', 'data': 4}, 9999999999L)
+        foo_new = items.next()
+        self.assertEqual('foo', foo_new['id'])
+
+if __name__ == '__main__':
+    unittest.main()
