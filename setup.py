a989b5073a93d22a90023a4235f7b66b9328114d
Alexander Shorin <kxepal@gmail.com>
2012-10-16 20:04:47 +0400

    Open next developing cycle.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name = 'phoxpy',
-    version = '0.2',
+    version = '0.3-dev',
     description = 'Python client to Laboratory Information System',
     long_description = \
 """This is a Python library that implements protocol and base operations with
