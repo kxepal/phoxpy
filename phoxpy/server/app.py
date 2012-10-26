275563f5b1ccc7abe65cb740c825302a79b4fbeb
Alexander Shorin <kxepal@gmail.com>
2012-10-26 16:28:41 +0400

    Add base dispatcher by message type.
    Current phox client realisation doesn't support http redirects
    and uses single /phox resource as main communication point. Since we
    are going to build modular system via blueprints, we need to
    implicitly reroute request based on message type to specific handler.

--- phoxpy/server/app.py
+++ phoxpy/server/app.py
@@ -7,9 +7,29 @@
 # you should have received as part of this distribution.
 #
 
-from flask import Flask
+from flask import Flask, request, abort
+from phoxpy import xml
 
 app = Flask(__name__)
+
+@app.before_request
+def dispatch():
+    if not request.headers['content_length']:
+        abort(400, 'payload required')
+    message = xml.decode(request.stream)
+    if message.type is None:
+        abort(400, 'message type is missed')
+    elif request.path == '/phox':
+        for rule in app.url_map.iter_rules():
+            if rule.rule.endswith(message.type):
+                request.path = rule.rule
+                request.url_rule = rule
+                request.phoxmsg = message
+                break
+        else:
+            abort(400, 'unknown type %r' % message.type)
+    else:
+        abort(404)
 
 @app.route('/phox', methods=['GET','POST'])
 def phox():
